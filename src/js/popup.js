3cd87661f36e5a7eb32f8cc70aa956d741ab7e1b
Steven Pham <stevenpham408@yahoo.com>
2020-07-26 16:03:22 -0700

    Rendering components

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,13 @@
-import "../css/popup.css";
-import Greeting from "./popup/greeting_component.jsx";
+import "../css/popup.scss";
 import React from "react";
 import { render } from "react-dom";
+// import GreetingComponent from './popup/greeting_component.jsx'
+// import BlockTitle from './popup/block_title_component.jsx'
+// import {AutoDelete} from './popup/auto_delete_component.jsx'
+import App from './popup/app_component.jsx';
 
 render(
-  <Greeting/>,
-  window.document.getElementById("app-container")
+  <App/>,
+  window.document.getElementById("app-container") 
 );
+    
