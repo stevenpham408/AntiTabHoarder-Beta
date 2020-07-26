296c5d009048e2096004428d17414ac7aa72c48c
Steven Pham <stevenpham408@yahoo.com>
2020-07-26 16:05:16 -0700

    App component that encapsulates other components

---
+++ src/js/popup/app_component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+import BlockTitleComponent from './block_title_component.jsx'
+import AutoDeleteComponent from './auto_delete_component.jsx'
+import DividerComponent from './divider_component.jsx'
+
+class AppComponent extends React.Component{
+    render() {
+        return (
+            <div>
+                <BlockTitleComponent/>
+                <DividerComponent text='Auto Delete'/>
+                {/* <AutoDeleteComponent/> */}
+
+                
+                
+            </div>
+        )
+    }
+};
+
+export default hot(module)(AppComponent)
