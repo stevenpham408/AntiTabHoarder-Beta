b326a3280077bc8e95d7a0a724dd31832a4b2fa8
Steven Pham <stevenpham408@yahoo.com>
2020-07-27 18:40:28 -0700

    Added child components to finish the block-styling

--- src/js/popup/auto_delete_component.jsx
+++ src/js/popup/auto_delete_component.jsx
@@ -1,36 +1,47 @@
 import React from "react";
     
 import { TextField } from '@material-ui/core/';
-import { Switch } from '@material-ui/core';
 import { hot } from "react-hot-loader";
+import ToggleSwitchComponent from './toggle_switch_component.jsx'
+import SelectTimeUnitComponent from './select_time_unit_component.jsx'
+import { Select } from '@material-ui/core'
 
+import { MenuItem } from '@material-ui/core'
 class AutoDeleteComponent extends React.Component{
     render () {
         return (
             <div id='block1'>
-                <span id='auto_delete_text'> Auto-Delete after </span> 
                 <TextField 
                     id="auto_delete_time" 
                     label="Amount of Time" 
                     type='number'
-                    style = {{ width: 50 }}
+                    variant='standard'
+                    style = 
+                    {{
+                        width: 50,
+                        marginRight: '25px'
+                    }}
                     inputProps={{
                         style: {
+                            height: 23,
                             fontSize: 14,
                         },
                     }}
                     InputLabelProps={{
                         style: {
-                            fontSize: 10
+                            fontSize: 13
                         },
                     }}
+                    
                     />
-                            
 
-            
-                <Switch id='auto_delete_toggle'></Switch>
-                
-            </div>
+                    <SelectTimeUnitComponent>
+                        <MenuItem value={'minutes'}>minutes</MenuItem>
+                        <MenuItem value={'hours'}>hours</MenuItem>
+                    </SelectTimeUnitComponent>
+                    <ToggleSwitchComponent/>
+
+             </div>
         )
     }
 };
