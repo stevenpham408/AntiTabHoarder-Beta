bb2822a5649da996e68b18f870ed3cd2a5e6e0d7
Steven Pham <stevenpham408@yahoo.com>
2020-07-31 00:00:54 -0700

    Made a separate component for the numerical field

--- src/js/popup/auto_delete_component.jsx
+++ src/js/popup/auto_delete_component.jsx
@@ -3,42 +3,15 @@ import React from "react";
 import { TextField } from '@material-ui/core/';
 import { hot } from "react-hot-loader";
 import ToggleSwitchComponent from './toggle_switch_component.jsx'
-import SelectTimeUnitComponent from './select_time_unit_component.jsx'
-import { Select } from '@material-ui/core'
+import SelectComponent from './select_time_unit_component.jsx'
+import TimeAmountComponent from './time_amount_component.jsx'
 
-import { MenuItem } from '@material-ui/core'
 class AutoDeleteComponent extends React.Component{
     render () {
         return (
             <div id='block1'>
-                <TextField 
-                    id="auto_delete_time" 
-                    label="Amount of Time" 
-                    type='number'
-                    variant='standard'
-                    style = 
-                    {{
-                        width: 50,
-                        marginRight: '25px'
-                    }}
-                    inputProps={{
-                        style: {
-                            height: 23,
-                            fontSize: 14,
-                        },
-                    }}
-                    InputLabelProps={{
-                        style: {
-                            fontSize: 13
-                        },
-                    }}
-                    
-                    />
-
-                    <SelectTimeUnitComponent>
-                        <MenuItem value={'minutes'}>minutes</MenuItem>
-                        <MenuItem value={'hours'}>hours</MenuItem>
-                    </SelectTimeUnitComponent>
+                    <TimeAmountComponent/>
+                    <SelectComponent id='sex'/>
                     <ToggleSwitchComponent/>
 
              </div>
