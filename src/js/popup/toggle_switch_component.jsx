dca556d2aa0eb264a34aa9c26bdaa8440af7b8e9
Steven Pham <stevenpham408@yahoo.com>
2020-07-31 00:01:44 -0700

    Implemented component's state and handling

--- src/js/popup/toggle_switch_component.jsx
+++ src/js/popup/toggle_switch_component.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { hot } from "react-hot-loader";
 
 import { Switch } from '@material-ui/core/';
+
 
 // The `withStyles()` higher-order component is injecting a `classes`
 // prop that is used by the `Button` component.
@@ -12,10 +13,40 @@ const StyledSwitch = withStyles({
     marginTop: '20px',
     marginLeft: '90px'
   },
+  
 })(Switch);
 
 function ClassesShorthand() {
-  return <StyledSwitch></StyledSwitch>;
+
+  const [state, setState] = React.useState(undefined)
+ 
+  const handleChange = (event) => {
+
+    setState(event.target.checked);
+    chrome.storage.local.set({auto_delete_toggle: event.target.checked });
+  }
+
+  React.useEffect(() => {
+    
+    chrome.storage.local.get(null, function(res){
+      setState(res.auto_delete_toggle === undefined ? false : res.auto_delete_toggle);
+    });
+  }, []);
+
+  if(state === undefined) {
+    return null;
+  }
+
+  else{
+  return (
+
+    <StyledSwitch
+    checked={state}
+    onChange={handleChange}
+    > 
+    </StyledSwitch>)
+  }
+ 
 }
 
-export default hot(module)(StyledSwitch)
+export default hot(module)(ClassesShorthand)
