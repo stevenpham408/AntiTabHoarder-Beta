e64252766029d0e59eccecfbe5ec388914a98919
Steven Pham <stevenpham408@yahoo.com>
2020-08-07 02:13:09 -0700

    Removed debugging console statements

--- src/js/makeData.js
+++ src/js/makeData.js
@@ -13,13 +13,9 @@ export function getVarFromLocalStorage2(name){
 
 export function getVarFromLocalStorage(name){
     return new Promise(function(resolve, reject) {
-        // console.log("get: getting "+ name +" from localstorage")
         chrome.storage.local.get([name], function(items){
             let target = items[name];
-            console.log("length of target: ", target, "length of items[name]:", items[name] )
-            // console.log("   items[name] = " + target)
             let resultObj = Object.keys(target).map((key) => [Number(key), target[key]]);
-            alert('Access')
             resolve(resultObj)
         });
     })
