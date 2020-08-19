2b44741a1b0fff549597af87f31668ce03523203
Steven Pham <stevenpham408@yahoo.com>
2020-08-18 22:53:46 -0700

    Removed comment bloat

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,13 +1,10 @@
 import "../css/popup.scss";
 import React from "react";
+
 import { render } from "react-dom";
-// import GreetingComponent from './popup/greeting_component.jsx'
-// import BlockTitle from './popup/block_title_component.jsx'
-// import {AutoDelete} from './popup/auto_delete_component.jsx'
 import App from './popup/app_component.jsx';
 
-render(
+ render(
   <App/>,
   window.document.getElementById("app-container") 
 );
-    
